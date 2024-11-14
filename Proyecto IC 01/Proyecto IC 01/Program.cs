429b45065cbfb76c78705e6ace0e2ae199dba1e0
Eyson Cabrera Tapia <eysoncabreratapia09@gmail.com>
2024-11-14 12:18:49 -0500

    Segunda actualizacion : Implemente un sistema de ventas

--- Proyecto IC 01/Proyecto IC 01/Program.cs
+++ Proyecto IC 01/Proyecto IC 01/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("My Name is Eyson Cabrera Tapia.");
+Console.WriteLine("Implemente un sistema de ventas diomoiooooooooooooooo");
